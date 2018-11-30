19aaf6716c960093123d0eed11eab0da7422173d
stephenhales <stephen.hales@trintech.com>
2018-11-30 13:34:57 -0600

    TodoList - add validation service

--- TodoList/src/main/java/todo/user/service/UserServiceBean.java
+++ TodoList/src/main/java/todo/user/service/UserServiceBean.java
@@ -3,8 +3,10 @@ package todo.user.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import todo.common.ValidationService;
 import todo.exception.UserException;
 import todo.exception.ValidationException;
 import todo.user.model.User;
@@ -13,15 +15,14 @@ import todo.user.model.User;
 public class UserServiceBean {
 
 	public User createUser(String name) throws ValidationException, UserException {
-		//validation
-		isNameValid(name);
+		validateName(name);
 
 		User user = new User();
 		user.setName(name);
 		return user;
 	}
 
-	private void isNameValid(String name) throws ValidationException, UserException {
+	private List<String> validateName(String name) throws ValidationException, UserException {
 		List<String> errors = new ArrayList<>();
 		if(name == null){throw new UserException("Name is required"); }
 		if(name.isEmpty()){errors.add("Name is empty"); }
@@ -31,6 +32,13 @@ public class UserServiceBean {
 		if(!errors.isEmpty()){
 			throw new ValidationException(errors);
 		}
+		return errors;
+	}
+
+	private List<String> validateEmail(String email) {
+		List<String> errors = new ArrayList<>();
+
+		return errors;
 	}
 
 	private Boolean stringHasNumber(String string){
