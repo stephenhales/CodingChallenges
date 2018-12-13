9991dd489806a13e9bdd98d87670af94f9f47135
stephenhales <stephen.hales@trintech.com>
2018-12-13 16:37:45 -0600

    Fowler - add in unit tests for Movie object

--- Fowler-First-Example/src/test/java/CustomerTest.java
+++ Fowler-First-Example/src/test/java/CustomerTest.java
@@ -1,11 +1,5 @@
 import static org.junit.Assert.*;
 
-import java.util.Arrays;
-import java.util.Collection;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
 public class CustomerTest {
 
 
--- Fowler-First-Example/src/test/java/MovieTest.java
+++ Fowler-First-Example/src/test/java/MovieTest.java
@@ -1,4 +1,48 @@
-public class MovieTest {
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
 
+import org.junit.Test;
+
+public class MovieTest {
 	
+	private static String validTitle = "Bourne";
+	private static int validPriceCode = 1;
+
+	@Test
+	private void canGetPriceCode(){
+		//Arrange
+		Movie movie = new Movie(validTitle, validPriceCode);
+
+		//Act
+		int priceCode = movie.getPriceCode();
+
+		//Assert
+		assertThat(priceCode, is(validPriceCode));
+	}
+
+	@Test
+	private void canSetPriceCode(){
+		//Arrange
+		Movie movie = new Movie(validTitle, validPriceCode);
+		int newPriceCode = 2;
+
+		//Act
+		movie.setPriceCode(newPriceCode);
+
+		//Assert
+		assertThat(movie.getPriceCode(), is(newPriceCode));
+	}
+
+	@Test
+	private void canGetTitle(){
+		//Arrange
+		Movie movie = new Movie(validTitle, validPriceCode);
+
+		//Act
+		String priceCode = movie.getTitle();
+
+		//Assert
+		assertThat(priceCode, is(validTitle));
+	}
+
 }
