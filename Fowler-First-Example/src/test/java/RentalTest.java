85bea4a000691f7b56851929ada2e2007d5236df
stephenhales <stephen.james.hales@gmail.com>
2018-12-28 11:18:13 -0600

    Fowler - add unit tests for Rental class

--- Fowler-First-Example/src/test/java/RentalTest.java
+++ Fowler-First-Example/src/test/java/RentalTest.java
@@ -9,7 +9,11 @@ import static org.mockito.Mockito.mock;
 
 
 public class RentalTest {
+
 	private static int dayRented = 1;
+	private static String regularMovie = "Regular Movie";
+	private static String newReleaseMovie = "New Release Movie";
+	private static String childrenMovie = "Children Movie";
 
 	@Test
 	public void canGetDaysRented(){
@@ -35,5 +39,138 @@ public class RentalTest {
 
 		//Assert
 		assertThat(movieResult, is(movie));
+	}
+
+	@Test
+	public void canGetCost_ForRegularMovieRental_twoDays(){
+		//Arrange
+		Rental rental = mockRental(mockRegularMovie(),2);
+
+		//Act
+		double rentalCostResult = Rental.getCostForMovieRental(rental);
+
+		//Assert
+		double expectedCost = 2;
+		assertThat(rentalCostResult, is(expectedCost));
+	}
+
+	@Test
+	public void canGetCost_ForRegularMovieRental_threeDays(){
+		//Arrange
+		Rental rental = mockRental(mockRegularMovie(),3);
+
+		//Act
+		double rentalCostResult = Rental.getCostForMovieRental(rental);
+
+		//Assert
+		double expectedCost = 3.5;
+		assertThat(rentalCostResult, is(expectedCost));
+	}
+
+	@Test
+	public void canGetCost_ForNewReleaseMovieRental(){
+		//Arrange
+		Rental rental = mockRental(mockNewReleaseMovie(),2);
+
+		//Act
+		double rentalCostResult = Rental.getCostForMovieRental(rental);
+
+		//Assert
+		double expectedCost = 6;
+		assertThat(rentalCostResult, is(expectedCost));
+	}
+
+	@Test
+	public void canGetCost_ForChildrenMovieRental_threeDays(){
+		//Arrange
+		Rental rental = mockRental(mockChildrenMovie(),3);
+
+		//Act
+		double rentalCostResult = Rental.getCostForMovieRental(rental);
+
+		//Assert
+		double expectedCost = 1.5;
+		assertThat(rentalCostResult, is(expectedCost));
+	}
+
+	@Test
+	public void canGetCost_ForChildrenMovieRental_fiveDays(){
+		//Arrange
+		Rental rental = mockRental(mockChildrenMovie(),5);
+
+		//Act
+		double rentalCostResult = Rental.getCostForMovieRental(rental);
+
+		//Assert
+		double expectedCost = 4.5;
+		assertThat(rentalCostResult, is(expectedCost));
+	}
+
+	@Test
+	public void canGetRenterPoints_ForRegularMovieRental(){
+		//Arrange
+		Rental rental = mockRental(mockRegularMovie(),1);
+
+		//Act
+		int points = Rental.getFrequentRenterPointsForRental(rental);
+
+		//Assert
+		int expectedPoints = 1;
+		assertThat(points, is(expectedPoints));
+	}
+
+	@Test
+	public void canGetRenterPoints_ForNewReleaseMovieRental_OneDay(){
+		//Arrange
+		Rental rental = mockRental(mockNewReleaseMovie(),1);
+
+		//Act
+		int points = Rental.getFrequentRenterPointsForRental(rental);
+
+		//Assert
+		int expectedPoints = 1;
+		assertThat(points, is(expectedPoints));
+	}
+
+	@Test
+	public void canGetRenterPoints_ForNewReleaseMovieRental_TwoDays(){
+		//Arrange
+		Rental rental = mockRental(mockNewReleaseMovie(),2);
+
+		//Act
+		int points = Rental.getFrequentRenterPointsForRental(rental);
+
+		//Assert
+		int expectedPoints = 2;
+		assertThat(points, is(expectedPoints));
+	}
+
+	@Test
+	public void canGetRenterPointst_ForChildrenMovieRental() {
+		//Arrange
+		Rental rental = mockRental(mockChildrenMovie(),1);
+
+		//Act
+		int points = Rental.getFrequentRenterPointsForRental(rental);
+
+		//Assert
+		int expectedPoints = 1;
+		assertThat(points, is(expectedPoints));
+	}
+
+	private Rental mockRental(Movie movie, int days){
+		return new Rental(movie, days);
+	}
+
+	private Movie mockRegularMovie(){
+		return new Movie(regularMovie, Movie.REGULAR);
+	}
+
+	private Movie mockNewReleaseMovie(){
+		return new Movie(newReleaseMovie, Movie.NEW_RELEASE);
+	}
+
+	private Movie mockChildrenMovie(){
+		return new Movie(childrenMovie, Movie.CHILDREN);
 	}
 }
