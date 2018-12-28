69e941313406c163a9c76391fb31e50776d9bf33
stephenhales <stephen.james.hales@gmail.com>
2018-12-28 11:43:16 -0600

    fowler - refactor how the statement is created to avoid creating a new string each loop iteration. Pull out strings into methods to clean up the statement method.

--- Fowler-First-Example/src/main/java/refactored/Customer.java
+++ Fowler-First-Example/src/main/java/refactored/Customer.java
@@ -24,23 +24,39 @@ public class Customer {
 	public List<Rental> getRentals(){ return _rentals; }
 
 	public String statement() {
-
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
-		String statement = "Rental record for " + getName() + "\n";
+		List<String> statement = new ArrayList<>();
+
+		statement.add( headerStatement());
+
 		for(Rental rental: _rentals) {
 
 			double movieCost = Rental.getCostForMovieRental(rental);
-
 			frequentRenterPoints += Rental.getFrequentRenterPointsForRental(rental);
 
-			//show figures for this rental
-			statement += "\t" + rental.getMovie().getTitle()+ "\t" + String.valueOf(movieCost) + "\n";
+			statement.add( rentalStatement(rental.getMovie().getTitle(), movieCost));
 			totalAmount += movieCost;
 		}
-		//add footer lines
-		statement += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		statement += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
-		return statement;
+
+		statement.add(amountOwedStatement(totalAmount));
+		statement.add(frequentRenterPointsStatement(frequentRenterPoints));
+		return String.join("\n", statement);
+	}
+
+	private String headerStatement(){
+		return "Rental record for " + getName();
+	}
+
+	private String rentalStatement(String movieTitle, double movieCost){
+		return "\t" + movieTitle + "\t" + String.valueOf(movieCost) + "\n";
+	}
+
+	private String amountOwedStatement(double totalAmount){
+		return "Amount owed is " + String.valueOf(totalAmount);
+	}
+
+	private String frequentRenterPointsStatement(int frequentRenterPoints){
+		return "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 	}
 }
