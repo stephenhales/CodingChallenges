792dd05373ecb9d10c941a867ae817eac5726cba
stephenhales <stephen.james.hales@gmail.com>
2019-01-03 16:23:59 -0600

    Tennis - rename some variables, start to break out pieces of getScore

--- Tennis-Refactoring-Kata-master/java/src/main/java/tennisGame1/TennisGame1.java
+++ Tennis-Refactoring-Kata-master/java/src/main/java/tennisGame1/TennisGame1.java
@@ -4,8 +4,8 @@ import common.ITennisGame;
 
 public class TennisGame1 implements ITennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int player1Score = 0;
+    private int player2Score = 0;
     private String player1Name;
     private String player2Name;
 
@@ -16,36 +16,20 @@ public class TennisGame1 implements ITennisGame {
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            m_score1 += 1;
+            player1Score += 1;
         else
-            m_score2 += 1;
+            player2Score += 1;
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
+        if (player1Score == player2Score)
         {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+            return getEqualScore(player1Score);
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (player1Score >=4 || player2Score >=4)
         {
-            int minusResult = m_score1-m_score2;
+            int minusResult = player1Score - player2Score;
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
@@ -53,10 +37,11 @@ public class TennisGame1 implements ITennisGame {
         }
         else
         {
+            int tempScore=0;
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                if (i==1) tempScore = player1Score;
+                else { score+="-"; tempScore = player2Score;}
                 switch(tempScore)
                 {
                     case 0:
@@ -75,5 +60,19 @@ public class TennisGame1 implements ITennisGame {
             }
         }
         return score;
+    }
+
+    private String getEqualScore(int score){
+        switch (score)
+        {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            default:
+                return "Deuce";
+        }
     }
 }
