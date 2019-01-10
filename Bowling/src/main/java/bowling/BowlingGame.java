d9f0970c860c6c85892fd4314114d7d31b1163a0
stephenhales <stephen.james.hales@gmail.com>
2019-01-10 11:05:31 -0600

    Bowling - break out logic within the for loop in score

--- Bowling/src/main/java/bowling/BowlingGame.java
+++ Bowling/src/main/java/bowling/BowlingGame.java
@@ -5,8 +5,9 @@ import bowling.frame.TenthFrame;
 
 public class BowlingGame{
     private Frame[] frames = new Frame[]{
-        new Frame(), new Frame(), new Frame(), new Frame(), new Frame(),
-        new Frame(), new Frame(), new Frame(), new Frame(),
+        new Frame(), new Frame(), new Frame(),
+        new Frame(), new Frame(), new Frame(),
+        new Frame(), new Frame(), new Frame(),
         new TenthFrame()
     };
     private Boolean isFirstRoll = true;
@@ -14,35 +15,50 @@ public class BowlingGame{
 
     public int score(){
         int score = 0;
-        for(int frameNumber = 0; frameNumber < 10; frameNumber++){
-            Frame frame = frames[frameNumber];
-
-            //spare
-            if(frame.getFirstRoll() + frame.getFirstRoll() == 10 ){
-                score += frame.getFirstRoll() + frame.getSecondRoll();
-
-                if(frameNumber < 9) {
-	                score += frames[frameNumber + 1].getFirstRoll();
-                }
-                else{
-                	TenthFrame tenth = (TenthFrame) frames[frameNumber];
-	                score += tenth.getThirdRoll();
-                }
-            }
-
-            // normal score
-            else{
-	            score += frame.getFirstRoll() + frame.getSecondRoll();
-            }
+        for(int frameNumber = 1; frameNumber <= 9; frameNumber++){
+            score += getFrameScore(frameNumber);
         }
         //10th frame
+        score += getTenthFrameScore();
 
         return score;
     }
 
+    public int getFrameScore(int frameNumber){
+        Frame frame = getFrame(frameNumber);
+
+        //spare
+        if(frame.getFirstRoll() + frame.getFirstRoll() == 10 ){
+            return 10 + getFrame(frameNumber+1).getFirstRoll();
+        }
+
+        // normal score
+        else{
+            return frame.getFirstRoll() + frame.getSecondRoll();
+        }
+    }
+
+    public int getTenthFrameScore(){
+        TenthFrame frame = (TenthFrame) frames[9];
+
+        //spare
+        if(frame.getFirstRoll() + frame.getFirstRoll() == 10 ){
+            return 10 + frame.getThirdRoll();
+        }
+
+        // normal score
+        else{
+            return frame.getFirstRoll() + frame.getSecondRoll();
+        }
+    }
+
+    public Frame getFrame(int frameNumber){
+        return frames[frameNumber-1];
+    }
+
     public void roll(int knockedDownPins){
         if(frameIndex == 10){
-	        TenthFrame tenth = (TenthFrame) frames[frameIndex-1];
+	        TenthFrame tenth = (TenthFrame) frames[9];
 	        tenth.setThirdRoll(knockedDownPins);
         }
     	else if(isFirstRoll){
